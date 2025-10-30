04f4e92ad2cee824e0a67392c57e44ea895f59a2
gaels <gaels@DESKTOP-I0B2FMA>
2025-10-30 20:14:37 -0300

    Tire un berretin en M4.java

--- M4/src/main/java/CINEMARX/M4/M4.java
+++ M4/src/main/java/CINEMARX/M4/M4.java
@@ -11,8 +11,8 @@ package CINEMARX.M4;
 public class M4 {
 
     /**
-     * @param args the command line arguments
-     */
+     * @param args the command line arguments la puta madre que lo pario porque me duele tanto el culo
+     */ 
     public static void main(String args[]) {
         // TODO code application logic here
     }
